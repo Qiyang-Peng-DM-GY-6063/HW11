f04c76d8e283c3b49debaa1482a865bd524fe52c
potuitou <qpeng@risd.edu>
2024-11-25 16:37:57 -0500

    final

--- HW11.ino
+++ HW11.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+const int pins[] = {4, 5, 6, 7, 8, 3}; 
+const int numPins = 6;
 
+int currentLED = 0;  
+unsigned long startTime = 0;
+const unsigned long lightInterval = 10000;  
+const unsigned long period = 60000;
+
+void setup() {
+  Serial.begin(9600); 
+  
+  for (int i = 0; i < numPins; i++) {
+    pinMode(pins[i], OUTPUT);
+    analogWrite(pins[i], 0); 
+  }
+  startTime = millis();
+  currentLED = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long now = millis(); 
 
+  // Restart after the period ends
+  if (now - startTime >= period) {
+    restart();
+    currentLED = 0;
+    startTime = now; 
+  }
+
+  if (now >= startTime + currentLED * lightInterval) {
+    for (int brightness = 0; brightness <= 255; brightness++) {
+      analogWrite(pins[currentLED], brightness);
+      delay(lightInterval / 256);
+
+      Serial.print("currentLED: ");
+      Serial.println(currentLED);
+      Serial.print("current pin: ");
+      Serial.println(pins[currentLED]);
+
+    }
+    currentLED++;                      
+  }
+
+}
+
+// Restart the 1-minute clock
+void restart() {
+  for (int i = 0; i < numPins; i++) {
+    analogWrite(pins[i], 0);
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HW11
  
+Use analog write to create a 60s clock by lighting up 6 leds one by one.
+
+Circuit Sketch
+ ![Page I](./circuit.jpg)
